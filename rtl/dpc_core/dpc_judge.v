a5cc14f60c464c6cfa7f8f84c2997c64401b4663
unknown <15021699275@163.com>
2026-02-18 19:13:30 +0800

    Modified the BSD-2 Simplified license

--- rtl/dpc_core/dpc_judge.v
+++ rtl/dpc_core/dpc_judge.v
@@ -1,6 +1,6 @@
 /**********************************************************************************************************
 *
-*  Copyright (c) 2025, Biggestapple
+*  Copyright (c) 2026, Biggest_apple
 *  All rights reserved.
 *
 *  Redistribution and use in source and binary forms, with or without modification, are permitted
